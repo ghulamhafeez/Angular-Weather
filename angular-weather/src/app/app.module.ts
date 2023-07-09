74d2038f0bf3a5bb12676c579cdc3701c5fd0bd4
Ghulam Muhammad <ghulamhafeez.dev@gmail.com>
2023-07-10 01:37:31 +0500

    create home component

--- angular-weather/src/app/app.module.ts
+++ angular-weather/src/app/app.module.ts
@@ -4,17 +4,22 @@ import {MatCardModule} from '@angular/material/card';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
 import { Home } from './home';
 import { Header } from './header';
+import {Footer} from "./footer"
 @NgModule({
   declarations: [
-    AppComponent , Home ,Header
+    AppComponent , Home ,Header,Footer
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatCardModule
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule
     
   ],
   providers: [],
---
+++ angular-weather/src/app/footer.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'Footer',
+  templateUrl: './footer.html',
+  styleUrls: ['./app.component.css']
+})
+export class Footer {
+
+}
