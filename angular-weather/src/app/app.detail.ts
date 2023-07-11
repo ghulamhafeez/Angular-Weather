d35f8cc4e459b343b7aeb6c3290906d61f59c338
Ghulam Muhammad <ghulamhafeez.dev@gmail.com>
2023-07-11 22:22:55 +0500

    Fixed issue in app detail

--- angular-weather/src/app/app.detail.ts
+++ angular-weather/src/app/app.detail.ts
@@ -1,24 +1,27 @@
 import { Component } from '@angular/core';
 import { WeatherService } from './weather.services';
-import { ActivatedRoute, Route } from '@angular/router'
+// import { ActivatedRoute, Route } from '@angular/router'
 @Component({
   selector: 'Detail',
   templateUrl: './app.detail.html',
   styleUrls: ['./app.detail.css']
 })
 export class AppDetail {
-  constructor(private service: WeatherService ,private route: Route) {}
+  constructor(private service: WeatherService 
+    // ,private route: Route ,
+    // private ActivatedRoute: Route
+    ) {}
 
   // locationsForecast: any = this.service.getlocationsForecast();
 
   
-    ngOnInit() {
+    // ngOnInit() {
 
     // this.route.params.subscribe( params => console.log(params.id));
-    this.route.paramMap.switchMap((params: ParamMap) => {
-      let user_id = params.get('id');
+  //   this.route.paramMap.switchMap((params: ParamMap) => {
+  //     let user_id = params.get('id');
 
-      return this.userService.get(user_id);
-  })
-  }
+  //     return this.userService.get(user_id);
+  // })
+  // }
 }
